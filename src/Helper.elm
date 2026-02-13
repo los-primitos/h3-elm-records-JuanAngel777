7bcd7fc8e125ff70ad1c0adb547b60d0e5a6ae3d
JuanAngel777 <up230256@alumnos.upa.edu.mx>
2026-02-13 15:04:52 -0600

    Update Helper.elm

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,121 @@
-module Helper exposing (..)
+module Main exposing (main)
+
+import Html
+
+-- 1
+
+type alias ProgrammingLanguage =
+    { name : String
+    , releaseYear : Int
+    , currentVersion : String
+    }
+
+
+languages : List ProgrammingLanguage
+languages =
+    [ { name = "elm"
+      , releaseYear = 2012
+      , currentVersion = "0.19.1"
+      }
+    , { name = "javascript"
+      , releaseYear = 1995
+      , currentVersion = "ECMAScript 2025"
+      }
+    ]
+
+
+languageNames : List ProgrammingLanguage -> List String
+languageNames list =
+    List.map .name list
+
+-- 2
+
+type alias User =
+    { name : String
+    , uType : String
+    }
+
+
+users : List User
+users =
+    [ { name = "Roberto", uType = "Student" }
+    , { name = "Mitsiu", uType = "Professor" }
+    ]
+
+
+onlyStudents : List User -> List String
+onlyStudents list =
+    List.map
+        (\user ->
+            if .uType user == "Student" then
+                .name user
+            else
+                ""
+        )
+        list
+
+-- 3
+
+type alias Videogame =
+    { title : String
+    , releaseYear : Int
+    , available : Bool
+    , downloads : Int
+    , genres : List String
+    }
+
+
+videogames : List Videogame
+videogames =
+    [ { title = "Control"
+      , releaseYear = 2019
+      , available = True
+      , downloads = 5000000
+      , genres = [ "Action", "Shooter" ]
+      }
+    , { title = "Ocarina of Time"
+      , releaseYear = 1998
+      , available = True
+      , downloads = 8000000
+      , genres = [ "Action", "Adventure" ]
+      }
+    ]
+
+
+getVideogameGenres : List Videogame -> List (List String)
+getVideogameGenres list =
+    List.map .genres list
+
+-- 4
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "16GB"
+    , model = "IdeaPad 3"
+    , brand = "Lenovo"
+    , screenSize = "15.6"
+    }
+
+
+main : Html.Html msg
+main =
+    Html.div []
+        [ Html.h1 [] [ Html.text "My laptop" ]
+        , Html.div []
+            [ Html.ul []
+                [ Html.li [] [ Html.text ("Ram: " ++ .ram myLaptop) ]
+                , Html.li [] [ Html.text ("Modelo: " ++ .model myLaptop) ]
+                , Html.li [] [ Html.text ("Marca: " ++ .brand myLaptop) ]
+                , Html.li [] [ Html.text ("Pulgadas: " ++ .screenSize myLaptop) ]
+                ]
+            ]
+        ]
+
